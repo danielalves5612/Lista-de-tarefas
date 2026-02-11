8877d7bf198618bd9f81a27f7be4e11c173627d9
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-02-11 18:33:12 -0300

    Ajustes no botão editar

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,24 @@ const overlay = document.querySelector('.overlay')
 const modal = document.querySelector('.modal')
 const btnCancelar = document.querySelector('.btn-cancelar')
 const btnSalvar = document.querySelector('.btn-salvar')
+const editTitulo = document.querySelector('#edit-titulo')
+const editDescricao = document.querySelector('#edit-descricao')
+const editStatus = document.querySelector('#edit-status')
+const formEditar = document.querySelector('#form-editar')
 
 function criaTarefa(tituloValor, descricaoValor, statusValor){
     const section = document.createElement('section')
     section.setAttribute('class', 'tarefa')
     container.appendChild(section)
-    criaCampo(section, 'Tarefa', tituloValor)
-    criaCampo(section, 'Descrição', descricaoValor)
-    criaCampo(section, 'Status', statusValor)
+    criaCampo(section, 'Tarefa', tituloValor, 'titulo')
+    criaCampo(section, 'Descrição', descricaoValor, 'descricao')
+    criaCampo(section, 'Status', statusValor, 'status')
     criaAcoes(section)
 }
 
-function criaCampo(section, titulo, valor){
+function criaCampo(section, titulo, valor, tipo){
     const div = document.createElement('div')
-    div.setAttribute('class', 'campo')
+    div.setAttribute('class', `campo campo--${tipo}`)
     section.appendChild(div)
     const h2 = document.createElement('h2')
     h2.setAttribute('class', 'campo-titulo')
@@ -70,15 +74,17 @@ document.addEventListener('click', function(e){
     if(el.classList.contains('button-editar')){
         e.preventDefault()
         tarefaEmEdicao = el.closest('.tarefa')
+        const tituloAtual = tarefaEmEdicao.querySelector('.campo--titulo p').innerText
+        const descricaoAtual = tarefaEmEdicao.querySelector('.campo--descricao p').innerText
+        const statusAtual = tarefaEmEdicao.querySelector('.campo--status p').innerText
+        editTitulo.value = tituloAtual
+        editDescricao.value = descricaoAtual
+        editStatus.value = statusAtual.toLowerCase()
         abrirModal()
     }
 
     if(el.classList.contains('btn-cancelar')){
         e.preventDefault()
-        fecharModal()
-    }
-
-    if(el.classList.contains('btn-salvar')){
         fecharModal()
     }
 
@@ -90,4 +96,15 @@ document.addEventListener('click', function(e){
 formTarefa.addEventListener('submit', function(e){
     e.preventDefault()
     criaTarefa(inputTitulo.value, inputDescricao.value, inputStatus.value)
+})
+
+formEditar.addEventListener('submit', function(e){
+    e.preventDefault()
+    const tituloEditado = tarefaEmEdicao.querySelector('.campo--titulo p')
+    const descricaoEditado = tarefaEmEdicao.querySelector('.campo--descricao p')
+    const statusEditado = tarefaEmEdicao.querySelector('.campo--status p')
+    tituloEditado.innerText = editTitulo.value
+    descricaoEditado.innerText = editDescricao.value
+    statusEditado.innerText = editStatus.value
+    fecharModal()
 })
