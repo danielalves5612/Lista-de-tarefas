7e184d1d8295156667f9a24f7de5716867a96f2e
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-02-11 16:18:10 -0300

    Criado overlay, modal e novas funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,62 +4,89 @@ const inputDescricao = document.querySelector('#input-descricao')
 const inputStatus = document.querySelector('#input-status')
 const buttonTarefa = document.querySelector('#button-tarefa')
 const container = document.querySelector('div#container')
+let tarefaEmEdicao
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+const btnCancelar = document.querySelector('.btn-cancelar')
+const btnSalvar = document.querySelector('.btn-salvar')
 
-function criaTarefa(inputTitulo, inputDescricao, inputStatus){
+function criaTarefa(tituloValor, descricaoValor, statusValor){
     const section = document.createElement('section')
     section.setAttribute('class', 'tarefa')
     container.appendChild(section)
+    criaCampo(section, 'Tarefa', tituloValor)
+    criaCampo(section, 'Descrição', descricaoValor)
+    criaCampo(section, 'Status', statusValor)
+    criaAcoes(section)
+}
+
+function criaCampo(section, titulo, valor){
     const div = document.createElement('div')
     div.setAttribute('class', 'campo')
     section.appendChild(div)
     const h2 = document.createElement('h2')
     h2.setAttribute('class', 'campo-titulo')
     div.appendChild(h2)
-    h2.innerHTML = 'Tarefa'
+    h2.innerHTML = titulo
     const p = document.createElement('p')
     div.appendChild(p)
-    p.innerHTML = inputTitulo
-
-    const divCampo = document.createElement('div')
-    divCampo.setAttribute('class', 'campo')
-    section.appendChild(divCampo)
-    const h2Campo = document.createElement('h2')
-    h2Campo.setAttribute('class', 'campo-titulo')
-    divCampo.appendChild(h2Campo)
-    h2Campo.innerHTML = 'Descrição'
-    const pCampo = document.createElement('p')
-    divCampo.appendChild(pCampo)
-    pCampo.innerHTML = inputDescricao
-
-    const divCampoS = document.createElement('div')
-    divCampoS.setAttribute('class', 'campo')
-    section.appendChild(divCampoS)
-    const h2CampoS = document.createElement('h2')
-    h2CampoS.setAttribute('class', 'campo-titulo')
-    h2CampoS.innerHTML = 'Status'
-    divCampoS.appendChild(h2CampoS)
-    const pCampoS = document.createElement('p')
-    divCampoS.appendChild(pCampoS)
-    pCampoS.innerHTML = inputStatus
-
-    const divCampoA = document.createElement('div')
-    divCampoA.setAttribute('class', 'campo')
-    section.appendChild(divCampoA)
-    const h2CampoA = document.createElement('h2')
-    h2CampoA.setAttribute('class', 'campo-titulo')
-    divCampoA.appendChild(h2CampoA)
-    h2CampoA.innerHTML = 'Ações'
-    const pCampoA = document.createElement('p')
-    divCampoA.appendChild(pCampoA)
-    const divButtonA = document.createElement('button')
-    divButtonA.setAttribute('class', 'button-editar')
-    divButtonA.innerHTML = 'Editar'
-    pCampoA.appendChild(divButtonA)
-    const divButtonB = document.createElement('button')
-    divButtonB.setAttribute('class', 'button-excluir')
-    divButtonB.innerHTML = 'Excluir'
-    pCampoA.appendChild(divButtonB)
+    p.innerHTML = valor
 }
+
+function criaAcoes(section, titulo){
+    const divAcao = document.createElement('div')
+    divAcao.setAttribute('class', 'campo')
+    section.appendChild(divAcao)
+    const h2Acao = document.createElement('h2')
+    h2Acao.setAttribute('class', 'campo-titulo')
+    divAcao.appendChild(h2Acao)
+    h2Acao.innerHTML = 'Ações'
+    const pAcao = document.createElement('p')
+    divAcao.appendChild(pAcao)
+    const botaoEditar = document.createElement('button')
+    botaoEditar.setAttribute('class', 'button-editar')
+    botaoEditar.innerHTML = 'Editar'
+    pAcao.appendChild(botaoEditar)
+    const botaoApagar = document.createElement('button')
+    botaoApagar.setAttribute('class', 'button-excluir')
+    botaoApagar.innerHTML = 'Excluir'
+    pAcao.appendChild(botaoApagar)
+}
+
+function abrirModal(){
+    overlay.classList.remove('hidden')
+}
+
+function fecharModal(){
+    overlay.classList.add('hidden')
+}
+
+document.addEventListener('click', function(e){
+    const el = e.target
+    if(el.classList.contains('button-excluir')){
+        el.closest('.tarefa').remove()
+    }
+
+    if(el.classList.contains('button-editar')){
+        e.preventDefault()
+        tarefaEmEdicao = el.closest('.tarefa')
+        abrirModal()
+    }
+
+    if(el.classList.contains('btn-cancelar')){
+        e.preventDefault()
+        fecharModal()
+    }
+
+    if(el.classList.contains('btn-salvar')){
+        fecharModal()
+    }
+
+    if(el.classList.contains('overlay')){
+        fecharModal()
+    }
+})
+
 formTarefa.addEventListener('submit', function(e){
     e.preventDefault()
     criaTarefa(inputTitulo.value, inputDescricao.value, inputStatus.value)
